b86ca0e2f3d9a3ddc66a3a027f78612253974241
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-09-13 17:24:41 -0700

    added pine

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
 
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
-
 let pineTree = document.createElement('img')
 pineTree.src = 'assets/pine-tree.png'
 pineTree.style.position = 'fixed'
