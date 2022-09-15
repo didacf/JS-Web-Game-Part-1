38225884c2d25319ccf75a642d2248de89c19656
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-09-14 18:59:08 -0700

    final activity web 2

--- index.js
+++ index.js
@@ -7,28 +7,30 @@ function tile(url, left, bottom, width, height){
 }
 
 function newImage(url, left, bottom){
-    let picture = document.createElement('img')
-    picture.src = 'url'
-    picture.style.position = 'fixed'
-    picture.style.left = left +'100px'
-    picture.style.bottom = bottom + 'px'
-    document.body.append(picture)
-    return picture
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
 }
 
 function newItem(url, left, bottom){
-    let picture = newImage(url, left, bottom)
+    let object = newImage(url, left, bottom)
 
-    picture.addEventListener('dblclick', () =>{
-        picture.remove()
+    object.addEventListener('dblclick', () => {
+        object.remove()
     })
 }
+
 let horizon = window.innerHeight / 1.75
 let heightOfSky = window.innerHeight-horizon
 let heightOfGrass = horizon
 
 tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
 tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
